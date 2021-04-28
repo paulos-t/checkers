2d89401829e3c12f4d7b7d9228fee92dd688df84
paulos-t <paulost72@gmail.com>
2021-04-28 03:10:44 -0400

    Ready for testing possible_jump_moves

--- main.py
+++ main.py
@@ -33,14 +33,17 @@ class CheckersCLI():
                 new_turn = False
                 continue
 
-            possible_moves = self.game.possible_moves(p_to_move)
-            if len(possible_moves) == 0:
-                print("That piece cannot move")
-                new_turn = False
-                continue
+            # possible_moves = self.game.possible_moves(p_to_move)
+            # if len(possible_moves) == 0:
+            #     print("That piece cannot move")
+            #     new_turn = False
+            #     continue
             else:
-                selected_move = input(possible_moves)
+                # selected_move = input(possible_moves)
                 ## make a move
+                print(self.game.possible_basic_moves)
+                print(self.game.possible_jump_moves)
+                self.game.turn += 1
                 new_turn = True
 
 
@@ -51,7 +54,7 @@ class CheckerBoard():
     w_space = '\u25fb'
 
     def __init__(self):
-        self.board = [[Piece('b'), self.b_space, Piece('b'),self.b_space, Piece('b'), self.b_space, Piece('b'), self.b_space],
+        self.board = [[Piece('b'), self.b_space, Piece('b'), self.b_space, Piece('b'), self.b_space, Piece('b'), self.b_space],
             [self.b_space, Piece('b'), self.b_space, Piece('b'), self.b_space, Piece('b'), self.b_space, Piece('b')],
             [Piece('b'), self.b_space, Piece('b'), self.b_space, Piece('b'), self.b_space, Piece('b'), self.b_space],
             [self.b_space, self.w_space, self.b_space, self.w_space, self.b_space, self.w_space, self.b_space, self.w_space],
@@ -92,48 +95,191 @@ class CheckerBoard():
     def is_current_player_piece(self, piece:str) -> bool:
         coord = self.convert_checker_coord(piece)
         piece_color = str(self.board[coord[0]][coord[1]])
-        current_player = W_PEASANT if self.turn % 2 == 1 else B_PEASANT
-        return True if piece_color == current_player else False
+        if self.turn % 2 == 1:
+            if piece_color == W_PEASANT or piece_color == W_KING:
+                return True
+            else:
+                return False
+        elif self.turn % 2 == 0:
+            if piece_color == B_PEASANT or piece_color == B_KING:
+                return True
+            else:
+                return False
 
-    def can_move(self, piece:str) -> bool:
+    def possible_basic_moves(self, piece:str) -> str:
         coord = self.convert_checker_coord(piece)
+        piece_color = str(self.board[coord[0]][coord[1]])
+        p_basic_moves = []
         
-        #check if this is a black piece
-        if self.board[coord[0]][coord[1]].color == B_PEASANT:
-            #check for if a black piece is on the left edge of the board
+        if piece_color == B_PEASANT:
             if coord[1] == 0 and self.board[coord[0]+1][coord[1]+1] == self.w_space:
-                return True
-            #check for if a black piece is on the right edge of the board
+                # when in left column
+                p_basic_moves.append(self.convert_matrix_coord((coord[0]+1, coord[1]+1)))
             elif coord[1] == 7 and self.board[coord[0]+1][coord[1]-1] == self.w_space:
-                return True
-            elif self.board[coord[0]+1][coord[1]-1] == self.w_space:
-                return True
-            elif self.board[coord[0]+1][coord[1]+1] == self.w_space:
-                return True
+                # when in right column
+                p_basic_moves.append(self.convert_matrix_coord((coord[0]+1, coord[1]-1)))
             else:
-                return False
+                # everywhere else
+                if self.board[coord[0]+1][coord[1]-1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]+1, coord[1]-1)))
+                if self.board[coord[0]+1][coord[1]+1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]+1, coord[1]+1)))
 
-        #check if this is a white piece
-        elif self.board[coord[0]][coord[1]].color == W_PEASANT:
-            #check for if a white piece is on the left edge of a board
+        elif piece_color == W_PEASANT:
             if coord[1] == 0 and self.board[coord[0]-1][coord[1]+1] == self.w_space:
-                return True
-            #check for if a white piece is on the right edge of the board
+                # when in right column
+                p_basic_moves.append(self.convert_matrix_coord((coord[0]-1, coord[1]+1)))
             elif coord[1] == 7 and self.board[coord[0]-1][coord[1]-1] == self.w_space:
-                return True
-
-            elif self.board[coord[0]-1][coord[1]-1] == self.w_space:
-                return True
-            elif self.board[coord[0]-1][coord[1]+1] == self.w_space:
-                return True
+                # when in left column
+                p_basic_moves.append(self.convert_matrix_coord((coord[0]-1, coord[1]-1)))
             else:
-                return False
-        else:
-            return False
+                # everywhere else
+                if self.board[coord[0]-1][coord[1]-1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]-1, coord[1]-1)))
+                if self.board[coord[0]-1][coord[1]+1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]-1, coord[1]+1)))
+
+        elif piece_color == B_KING or piece_color == W_KING:
+            if coord[0] == 0 and coord[1] == 0 and self.board[coord[0]+1][coord[1]+1] == self.w_space:
+                # top-left corner
+                p_basic_moves.append(self.convert_matrix_coord((coord[0]+1, coord[1]+1)))
+            elif coord[0] == 7 and coord[1] == 7 and self.board[coord[0]-1][coord[1]-1] == self.w_space:
+                # bottom-right corner
+                p_basic_moves.append(self.convert_matrix_coord((coord[0]-1, coord[1]-1)))
+            elif coord[0] == 0:
+                # when king is in top row
+                if self.board[coord[0]+1][coord[1]-1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]+1, coord[1]-1)))
+                if self.board[coord[0]+1][coord[1]+1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]+1, coord[1]+1)))
+            elif coord[0] == 7:
+                # when king is in bottom row
+                if self.board[coord[0]-1][coord[1]-1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]-1, coord[1]-1)))
+                if self.board[coord[0]-1][coord[1]+1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]-1, coord[1]+1)))
+            elif coord[1] == 0:
+                # when king is in left column
+                if self.board[coord[0]-1][coord[1]+1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]-1, coord[1]+1)))
+                if self.board[coord[0]+1][coord[1]+1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]+1, coord[1]+1)))
+            elif coord[1] == 7:
+                # when king is in right column
+                if self.board[coord[0]-1][coord[1]-1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]-1, coord[1]-1)))
+                if self.board[coord[0]+1][coord[1]-1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]+1, coord[1]-1)))
+            else:
+                # everywhere else (middle of board)
+                if self.board[coord[0]-1][coord[1]-1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]-1, coord[1]-1)))
+                if self.board[coord[0]-1][coord[1]+1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]-1, coord[1]+1)))
+                if self.board[coord[0]+1][coord[1]-1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]+1, coord[1]-1)))
+                if self.board[coord[0]+1][coord[1]+1] == self.w_space:
+                    p_basic_moves.append(self.convert_matrix_coord((coord[0]+1, coord[1]+1)))
+
+        return p_basic_moves
+
+    def possible_jump_moves(self, piece:str) -> str:
+        coord = self.convert_checker_coord(piece)
+        piece_color = str(self.board[coord[0]][coord[1]])
+        p_jump_moves = [] # [ [new location of moved piece, captured piece location] [ , ] ... ]
+
+        down_right = self.convert_matrix_coord( (coord[0]+1, coord[1]+1) )
+        down_left = self.convert_matrix_coord( (coord[0]+1, coord[1]-1) )
+        up_right = self.convert_matrix_coord( (coord[0]-1, coord[1]+1) )
+        up_left = self.convert_matrix_coord( (coord[0]-1, coord[1]-1) )
+        down_right_2 = self.convert_matrix_coord( (coord[0]+2, coord[1]+2) )
+        down_left_2 = self.convert_matrix_coord( (coord[0]+2, coord[1]-2) )
+        up_right_2 = self.convert_matrix_coord( (coord[0]-2, coord[1]+2) )
+        up_left_2 = self.convert_matrix_coord( (coord[0]-2, coord[1]-2) )
+
+        if piece_color == B_PEASANT:
+            if coord[0] == 7 or coord[0] == 6:
+                # when in bottom two rows
+                return p_jump_moves
+            elif (coord[1] == 0 or coord[1] == 1) and self.has_piece(down_right) and not self.is_current_player_piece(down_right) and not self.has_piece(down_right_2):
+                # when in first two columns
+                p_jump_moves.append( [down_right_2, down_right] )
+            elif (coord[1] == 7 or coord[1] == 6) and self.has_piece(down_left) and not self.is_current_player_piece(down_left) and not self.has_piece(down_left_2):
+                # when in last two columns
+                p_jump_moves.append( [down_left_2, down_left] )
+            else:
+                # everywhere else
+                if self.has_piece(down_right) and not self.is_current_player_piece(down_right) and not self.has_piece(down_right_2):
+                    p_jump_moves.append( [down_right_2, down_right] )
+                if self.has_piece(down_left) and not self.is_current_player_piece(down_left) and not self.has_piece(down_left_2):
+                    p_jump_moves.append( [down_left_2, down_left] )
         
-            
-
-
+        elif piece_color == W_PEASANT:
+            if coord[0] == 0 or coord[0] == 1:
+                # when in top two rows
+                return p_jump_moves
+            elif (coord[1] == 0 or coord[1] == 1) and self.has_piece(up_right) and not self.is_current_player_piece(up_right) and not self.has_piece(up_right_2):
+                # when in first two columns
+                p_jump_moves.append( [up_right_2, up_right] )
+            elif (coord[1] == 7 or coord[1] == 6) and self.has_piece(up_left) and not self.is_current_player_piece(up_left) and not self.has_piece(up_left_2):
+                # when in last two columns
+                p_jump_moves.append( [up_left_2, up_left] )
+            else:
+                # everywhere else
+                if self.has_piece(up_right) and not self.is_current_player_piece(up_right) and not self.has_piece(up_right_2):
+                    p_jump_moves.append( [up_right_2, up_right] )
+                if self.has_piece(up_left) and not self.is_current_player_piece(up_left) and not self.has_piece(up_left_2):
+                    p_jump_moves.append( [up_left_2, up_left] )
+        
+        elif piece_color == B_KING or piece_color == W_KING:
+            if ((coord[0] == 0 and coord[1] == 0) or (coord[0] == 1 and coord[1] == 1)) and self.has_piece(down_right) and not self.is_current_player_piece(down_right) and not self.has_piece(down_right_2):
+                # top left corner
+                p_jump_moves.append( [down_right_2, down_right] )
+            elif ((coord[0] == 7 and coord[1] == 7) or (coord[0] == 6 and coord[1] == 6)) and self.has_piece(up_left) and not self.is_current_player_piece(up_left) and not self.has_piece(up_left_2):
+                # bottom right corner
+                p_jump_moves.append( [up_left_2, up_left] )
+            elif ((coord[0] == 6 and coord[1] == 0) or (coord[0] == 7 and coord[1] == 1)) and self.has_piece(up_right) and not self.is_current_player_piece(up_right) and not self.has_piece(up_right_2):
+                # when king is on a7 or b8
+                p_jump_moves.append( [up_right_2, up_right] )
+            elif ((coord[0] == 0 and coord[1] == 6) or (coord[0] == 1 and coord[1] == 7)) and self.has_piece(down_left) and not self.is_current_player_piece(down_left) and not self.has_piece(down_left_2):
+                # when king is on g1 or h2
+                p_jump_moves.append( [down_left_2, down_left] )
+            elif coord[0] == 0 or coord[0] == 1:
+                # when king is in top two rows
+                if self.has_piece(down_right) and not self.is_current_player_piece(down_right) and not self.has_piece(down_right_2):
+                    p_jump_moves.append( [down_right_2, down_right] )
+                if self.has_piece(down_left) and not self.is_current_player_piece(down_left) and not self.has_piece(down_left_2):
+                    p_jump_moves.append( [down_left_2, down_left] )
+            elif coord[0] == 7 or coord[0] == 6:
+                # when king is in bottom two rows
+                if self.has_piece(up_right) and not self.is_current_player_piece(up_right) and not self.has_piece(up_right_2):
+                    p_jump_moves.append( [up_right_2, up_right] )
+                if self.has_piece(up_left) and not self.is_current_player_piece(up_left) and not self.has_piece(up_left_2):
+                    p_jump_moves.append( [up_left_2, up_left] )
+            elif coord[1] == 0 or coord[1] == 1:
+                # when king is in first two columns
+                if self.has_piece(up_right) and not self.is_current_player_piece(up_right) and not self.has_piece(up_right_2):
+                    p_jump_moves.append( [up_right_2, up_right] )
+                if self.has_piece(down_right) and not self.is_current_player_piece(down_right) and not self.has_piece(down_right_2):
+                    p_jump_moves.append( [down_right_2, down_right] )
+            elif coord[1] == 7 or coord[1] == 6:
+                # when king is in last two columns
+                if self.has_piece(up_left) and not self.is_current_player_piece(up_left) and not self.has_piece(up_left_2):
+                    p_jump_moves.append( [up_left_2, up_left] )
+                if self.has_piece(down_left) and not self.is_current_player_piece(down_left) and not self.has_piece(down_left_2):
+                    p_jump_moves.append( [down_left_2, down_left] )
+            else:
+                # everywhere else (middle of board)
+                if self.has_piece(down_right) and not self.is_current_player_piece(down_right) and not self.has_piece(down_right_2):
+                    p_jump_moves.append( [down_right_2, down_right] )
+                if self.has_piece(down_left) and not self.is_current_player_piece(down_left) and not self.has_piece(down_left_2):
+                    p_jump_moves.append( [down_left_2, down_left] )
+                if self.has_piece(up_right) and not self.is_current_player_piece(up_right) and not self.has_piece(up_right_2):
+                    p_jump_moves.append( [up_right_2, up_right] )
+                if self.has_piece(up_left) and not self.is_current_player_piece(up_left) and not self.has_piece(up_left_2):
+                    p_jump_moves.append( [up_left_2, up_left] )
+        
+        return p_jump_moves
 
     def possible_moves(self, piece:str) -> list:
         pass
@@ -147,13 +293,10 @@ class Piece():
             self.color = W_PEASANT
         else:
             self.color = None
-            print("Please provide a valid color")
+            print("Invalid color")
     
     def __repr__(self):
         return self.color
-
-    def can_move(self, piece:str) -> bool:
-        pass
 
     def move(self):
         pass
@@ -173,7 +316,7 @@ class King(Piece):
             self.color = W_KING
         else:
             self.color = None
-            print("Please provide a valid color")
+            print("Invalid color")
 
     def __repr__(self):
         return self.color
